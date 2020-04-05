84cb580a3bfd7c3681561e55c4cbe85292db40de
Gweanel Guyot <gguyot@pm.me>
2020-04-04 23:45:48 -0400

    rename /doc to /docs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require("path")
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "./doc"),
+  outputDir: path.resolve(__dirname, "./docs"),
   publicPath: "/heraldry-composer/",
 }
